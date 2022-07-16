e7f19933358f46b5417d83e2459f4ddfb844e85f
John Michael Bell <johnmichaelbell@gmail.com>
2022-07-15 22:11:44 -0400

    basic movement

--- scenes/die.gd
+++ scenes/die.gd
@@ -1,17 +1,28 @@
 extends Spatial
 
+var current_grid_pos: Vector2
+var target_grid_pos: Vector2
+var camera: Camera
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const MOVE_TIME = 0.1
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	current_grid_pos = Vector2(translation.x, translation.z).floor();
+	target_grid_pos = current_grid_pos;
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	if Input.is_key_pressed(KEY_UP):
-	pass
+	var target_pos = Vector3(target_grid_pos.x, 0, target_grid_pos.y)
+	if translation.is_equal_approx(target_pos):
+		translation = target_pos
+		if Input.is_action_just_pressed("ui_up"):
+			target_grid_pos += Vector2(0, -1)
+		if Input.is_action_just_pressed("ui_down"):
+			target_grid_pos += Vector2(0, 1)
+		if Input.is_action_just_pressed("ui_left"):
+			target_grid_pos += Vector2(-1, 0)
+		if Input.is_action_just_pressed("ui_right"):
+			target_grid_pos += Vector2(1, 0)
+	else:
+		translation = translation.move_toward(target_pos, delta / MOVE_TIME)
+	
+	
