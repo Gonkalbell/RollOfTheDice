7430672f13704f48cb1c6ca2b9bee27d20caf529
John Michael Bell <johnmichaelbell@gmail.com>
2022-07-16 05:09:21 -0400

    fixed dice rolling together

--- scenes/die.gd
+++ scenes/die.gd
@@ -1,10 +1,7 @@
 extends Spatial
+class_name Die
 
-var grid_pos: Vector2
-var orientation: Quat
-
-const MOVE_TIME = 0.1
-
+# Shamelessly stolen from http://kidscancode.org/godot_recipes/3d/rolling_cube/
 export var speed = 4.0
 
 onready var pivot = $Pivot
@@ -21,9 +18,38 @@ func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		roll(Vector3.RIGHT)
 
-func roll(dir):
+# Cast a ray before moving to check for obstacles
+func can_roll(dir: Vector3) -> bool:
+	var space = get_world().direct_space_state
+	var collision = space.intersect_ray(mesh.global_transform.origin, mesh.global_transform.origin + dir * 2.5, [self])
+	if collision:
+		if collision.collider.has_method("can_roll"):
+			# it's inefficient to make this recursive, but oh well
+			return collision.collider.can_roll(dir)
+		else:
+			return false
+	return true
+
+func match_neighbors():
+	var directions = [Vector3.FORWARD, Vector3.BACK, Vector3.LEFT, Vector3.RIGHT]
+	var space = get_world().direct_space_state
+	var top_face = get_top_face()
+	print(top_face)
+	for dir in directions:
+		var collision = space.intersect_ray(mesh.global_transform.origin, mesh.global_transform.origin + dir * 2, [self])
+		if collision and collision.collider.has_method("get_top_face"):
+			var other_top_face = collision.collider.get_top_face()
+			print(top_face, other_top_face)
+			if get_top_face() == collision.collider.get_top_face():
+				print("%s and %s both had face %d" % [self, collision.collider, get_top_face()])
+
+func roll(dir:Vector3):
 	# Do nothing if we're currently rolling.
 	if tween.is_active():
+		return
+
+	# Also do nothing if we can't roll this way
+	if not can_roll(dir):
 		return
 
 	## Step 1: Offset the pivot
@@ -45,8 +71,6 @@ func roll(dir):
 	mesh.transform.origin = Vector3(0, 1, 0)
 	mesh.global_transform.basis = basis
 
-	print_debug(get_top_face())
-
 func _on_Tween_tween_step(object, key, elapsed, value):
 	pivot.transform = pivot.transform.orthonormalized()
 
@@ -54,7 +78,6 @@ func get_top_face() -> int:
 	var top_face_dir = mesh.global_transform.basis.inverse() * Vector3.UP
 	var min_distance = 10000
 	var face_value = -1
-	print(top_face_dir)
 
 	var face_dirs = [Vector3.BACK, Vector3.RIGHT, Vector3.UP, Vector3.DOWN, Vector3.LEFT, Vector3.FORWARD]
 	
